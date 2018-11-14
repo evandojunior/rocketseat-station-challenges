1253af780d87ef6fd6edd3f62560f7c01a9921c3
Evando Junior <evando.junior@live.com>
2018-11-13 23:14:16 -0200

    First challenge

---
+++ challenge-js/01.js
@@ -0,0 +1,17 @@
+var endereco = {
+ rua: "Rua dos Pinheiros",
+ numero: 1293,
+ bairro: "Centro",
+ cidade: "São Paulo",
+ uf: "SP"
+};
+
+var example = "O usuário mora em São Paulo / SP, no bairro Centro, na rua \"Rua dos Pinheiros\" com nº 1293."
+
+function exibeEndereco(endereco){
+  return `O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua "${endereco.rua}" com nº ${endereco.numero}.`
+}
+
+console.log(example)
+console.log(exibeEndereco(endereco))
+example == exibeEndereco(endereco) ? console.log('Mission Complete') : console.log('Houston we have a problem!');
